94c88ead9cf69c51b60ef4a66ffbf1e2972e6788
Mutudu <aeondxf@live.com>
2026-04-13 16:26:57 +0800

    fix: tighten generator diagnostics and packaging metadata
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- src/PicoBench.Generators/GlobalUsings.cs
+++ src/PicoBench.Generators/GlobalUsings.cs
@@ -5,6 +5,7 @@ global using System.Collections.Generic;
 global using System.Collections.Immutable;
 global using System.Linq;
 global using System.Text;
+global using System.Threading;
 global using Microsoft.CodeAnalysis;
 global using Microsoft.CodeAnalysis.CSharp;
 global using Microsoft.CodeAnalysis.CSharp.Syntax;
